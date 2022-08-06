b613cc8ad8f75d6e543d15ff55f866c9782cd8df
AnbinX <klosyx@outlook.com>
2022-08-06 11:28:46 +0800

    Update dataset

--- src/datamodules/components/mix_modal_dataset.py
+++ src/datamodules/components/mix_modal_dataset.py
@@ -1,34 +1,31 @@
-import random
 from pathlib import Path
 
-import torch
-from torch import nn
 import numpy as np
 import pandas as pd
-from torchvision import transforms
+import torch
 from PIL import Image
+from torch import nn
+from torchvision import transforms
 
 # from data.transforms.randaug import RandAugment
 
 
-class GaussianBlur(object):
-    """blur a single image on CPU"""
+class GaussianBlur:
+    """blur a single image on CPU."""
 
     def __init__(self, kernel_size):
         radias = kernel_size // 2
         kernel_size = radias * 2 + 1
-        self.blur_h = nn.Conv2d(3, 3, kernel_size=(kernel_size, 1),
-                                stride=1, padding=0, bias=False, groups=3)
-        self.blur_v = nn.Conv2d(3, 3, kernel_size=(1, kernel_size),
-                                stride=1, padding=0, bias=False, groups=3)
+        self.blur_h = nn.Conv2d(
+            3, 3, kernel_size=(kernel_size, 1), stride=1, padding=0, bias=False, groups=3
+        )
+        self.blur_v = nn.Conv2d(
+            3, 3, kernel_size=(1, kernel_size), stride=1, padding=0, bias=False, groups=3
+        )
         self.k = kernel_size
         self.r = radias
 
-        self.blur = nn.Sequential(
-            nn.ReflectionPad2d(radias),
-            self.blur_h,
-            self.blur_v
-        )
+        self.blur = nn.Sequential(nn.ReflectionPad2d(radias), self.blur_h, self.blur_v)
 
         self.pil_to_tensor = transforms.ToTensor()
         self.tensor_to_pil = transforms.ToPILImage()
@@ -54,8 +51,19 @@ class GaussianBlur(object):
         return img
 
 
-class MixModalDataset(object):
-    def __init__(self, file_path, stage='fit', image_size=224, multimodal=False, mlm=False, whole_word_mask=False, eda=False, eda_prob=0.5, only_img=False):
+class MixModalDataset:
+    def __init__(
+        self,
+        file_path,
+        stage="fit",
+        image_size=224,
+        multimodal=False,
+        mlm=False,
+        whole_word_mask=False,
+        eda=False,
+        eda_prob=0.5,
+        only_img=False,
+    ):
         file_path = Path(file_path)
         if not file_path.exists():
             raise FileNotFoundError(f"{file_path} not found!")
@@ -71,22 +79,22 @@ class MixModalDataset(object):
         self.whole_word_mask = whole_word_mask
         self.eda = eda
         self.eda_prob = eda_prob
-        print('total data:', len(self.data))
+        print("total data:", len(self.data))
 
     @staticmethod
     def get_simclr_pipeline_transform(size, s=1):
         """Return a set of data augmentation transformations as described in the SimCLR paper."""
-        color_jitter = transforms.ColorJitter(
-            0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
-        data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=size),
-                                              transforms.RandomHorizontalFlip(),
-                                              transforms.RandomApply(
-                                                  [color_jitter], p=0.8),
-                                              transforms.RandomGrayscale(
-                                                  p=0.2),
-                                              GaussianBlur(
-                                                  kernel_size=int(0.1 * size)),
-                                              transforms.ToTensor()])
+        color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+        data_transforms = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(size=size),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomApply([color_jitter], p=0.8),
+                transforms.RandomGrayscale(p=0.2),
+                GaussianBlur(kernel_size=int(0.1 * size)),
+                transforms.ToTensor(),
+            ]
+        )
         return data_transforms
 
     def __len__(self):
@@ -96,10 +104,10 @@ class MixModalDataset(object):
         sample = self.data.iloc[index]
         text = sample["text"]
         # if use text agumentation
-        if self.eda and self.stage == 'fit':
-            p = random.random()
+        if self.eda and self.stage == "fit":
+            p = np.random.random()
             if p < self.eda_prob:
-                text = random.choice(sample['eda_text'])
+                text = np.random.choice(sample["eda_text"])
         if not self.mlm:
             label = sample["label"]
             # if use multimodal data
@@ -110,7 +118,7 @@ class MixModalDataset(object):
                     valid_img = True
                 else:
                     # dummy image, will be masked during training
-                    img = Image.new('RGB', (224, 224))
+                    img = Image.new("RGB", (224, 224))
                     valid_img = False
                 return self.transformer(img), text, valid_img, label
             else:
