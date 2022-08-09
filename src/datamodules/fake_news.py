02e69dfcc24e8f028eb934f656fe9da66361c524
AnbinX <klosyx@outlook.com>
2022-08-09 12:12:32 +0800

    Add fake news experiment

---
+++ src/datamodules/fake_news.py
@@ -0,0 +1,154 @@
+from pathlib import Path
+from typing import List
+
+import pandas as pd
+import pytorch_lightning as pl
+import torch
+from PIL import Image
+from pytorch_lightning.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+from transformers import AutoFeatureExtractor, AutoTokenizer
+
+
+class WeiboDataset:
+    def __init__(self, img_path, data_path) -> None:
+        assert isinstance(img_path, str) and isinstance(data_path, str), "path must be a string"
+        self.data = pd.read_json(data_path, lines=True)
+        self.img_path = Path(img_path)
+        #
+        self.transforms = transforms.Compose(
+            [
+                transforms.Resize(256, interpolation=transforms.InterpolationMode.BILINEAR),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        print("data size:", len(self.data))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        text = self.data.iloc[idx]["title"]
+        img_name = self.data.iloc[idx]["imgs"][0]
+        img_path = self.img_path / img_name
+        img = Image.open(img_path)
+        label = self.data.iloc[idx]["label"]
+        return text, self.transforms(img).unsqueeze(0), torch.tensor([label], dtype=torch.long)
+
+
+class Collector:
+    def __init__(self, tokenizer: str, processor: str, max_length: int = 200) -> None:
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer, cache_dir="/data/.cache")
+        self.processor = (
+            AutoFeatureExtractor.from_pretrained(processor, cache_dir="/data/.cache")
+            if processor is not None
+            else None
+        )
+        self.max_length = max_length
+
+    def __call__(self, data: List):
+        texts, imgs, labels = zip(*data)
+        text_encodeds = self.tokenizer(
+            list(texts),
+            padding=True,
+            truncation=True,
+            max_length=self.max_length,
+            return_tensors="pt",
+        )
+        img_encodeds = (
+            self.processor(imgs) if self.processor is not None else torch.cat(imgs, dim=0)
+        )
+        # image agumentation here, todo...
+        labels = torch.cat(labels)
+        return text_encodeds, img_encodeds, labels
+
+
+class MultiModalData(pl.LightningDataModule):
+    def __init__(
+        self,
+        img_path,
+        train_path,
+        test_path,
+        tokenizer_name="bert-base-chinese",
+        processor_name=None,
+        dataset_name="weibo",
+        max_length=200,
+        val_set_ratio: int = 0.1,
+        batch_size=32,
+        num_workers=0,
+    ) -> None:
+        super().__init__()
+        self.img_path = img_path
+        self.train_path = train_path
+        self.test_path = test_path
+        self.dataset_name = dataset_name
+        self.val_set_ratio = val_set_ratio
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.collate_fn = Collector(
+            tokenizer=tokenizer_name, processor=processor_name, max_length=max_length
+        )
+
+    def setup(self, stage: str = None):
+        if stage == "fit" or stage is None:
+            dataset = (
+                WeiboDataset(self.img_path, self.train_path)
+                if self.dataset_name == "weibo"
+                else None
+            )
+            total_size = len(dataset)
+            val_set_size = int(total_size * self.val_set_ratio)
+            print("total_size:", total_size - val_set_size, "val_set_size:", val_set_size)
+            self.train_dataset, self.val_dataset = random_split(
+                dataset, [total_size - val_set_size, val_set_size]
+            )
+
+        if stage == "test" or stage is None:
+            self.test_dataset = (
+                WeiboDataset(self.img_path, self.test_path)
+                if self.dataset_name == "weibo"
+                else None
+            )
+
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            collate_fn=self.collate_fn,
+        )
+
+    def val_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            collate_fn=self.collate_fn,
+        )
+
+    def test_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            collate_fn=self.collate_fn,
+        )
+
+
+# debug
+if __name__ == "__main__":
+    data = MultiModalData(
+        img_path="/data/fake_news/datasets/MM17-WeiboRumorSet/images",
+        train_path="/data/fake_news/datasets/MM17-WeiboRumorSet/train_data.json",
+        test_path="/data/fake_news/datasets/MM17-WeiboRumorSet/test_data.json",
+    )
+    data.setup("fit")
+    it = iter(data.train_dataloader())
+    while True:
+        item = next(it)
---
+++ src/models/spotfake.py
@@ -0,0 +1,88 @@
+import torch
+from torch import nn
+from torchvision.models import VGG19_BN_Weights, vgg19_bn
+from transformers import BertConfig, BertModel, get_constant_schedule_with_warmup
+
+from models.components.fake_news_base import FakeNewsBase
+
+
+class SpotFake(FakeNewsBase):
+    def __init__(
+        self,
+        lr=0.001,
+        weight_decay=0.05,
+        num_warmup_steps=400,
+        dropout_prob=0.0,
+        bert_name="bert-base-chinese",
+    ) -> None:
+        super().__init__()
+        self.save_hyperparameters()
+        bert_config = BertConfig.from_pretrained(bert_name, cache_dir="/data/.cache")
+        # model
+        self.bert = BertModel.from_pretrained(
+            bert_name, cache_dir="/data/.cache", config=bert_config
+        )
+        self.vgg_model = vgg19_bn(weights=VGG19_BN_Weights.IMAGENET1K_V1)
+
+        # layers without trainable weights
+        self.act = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_prob)
+
+        # fcs
+        self.img_fc1 = nn.Linear(self.vgg_model.classifier[6].in_features, 2742)
+        self.img_fc2 = nn.Linear(2742, 32)
+        self.text_fc1 = nn.Linear(self.bert.config.hidden_size, 32)
+        self.fc = nn.Linear(64, 35)
+        self.classifier = nn.Linear(35, 2)
+
+        # loss function
+        self.criterion = nn.CrossEntropyLoss()
+
+        # modify the last fc layer of vgg
+        new_classifier = torch.nn.Sequential(*list(self.vgg_model.children())[-1][:6])
+        self.vgg_model.classifier = new_classifier
+
+        # freeze vgg
+        self._freeze(self.vgg_model)
+
+        # freeze bert
+        self._freeze(self.bert)
+
+    def _freeze(self, module):
+        for n, p in module.named_parameters():
+            p.requires_grad = False
+            print("freeze", n)
+
+    def forward(self, text_encodeds, img_encodeds):
+        vgg_out = self.vgg_model(img_encodeds)
+        bert_out = self.bert(**text_encodeds).pooler_output
+
+        # visual encoding
+        x1 = self.img_fc1(vgg_out)
+        x1 = self.dropout(x1)
+        x1 = self.act(x1)
+        x1 = self.img_fc2(x1)
+        x1 = self.dropout(x1)
+        x1 = self.act(x1)  # (N, 32)
+
+        # text encoding
+        x2 = self.text_fc1(bert_out)
+        x2 = self.dropout(x2)
+        x2 = self.act(x2)  # (N, 32)
+
+        # multimodal repr
+        x = torch.cat([x1, x2], dim=1)  # (N, 64)
+        x = self.fc(x)
+        x = self.act(x)
+        logits = self.classifier(x)  # (N, 2)
+
+        return logits
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(
+            self.parameters(), lr=self.hparams.lr, weight_decay=self.hparams.weight_decay
+        )
+        scheduler = get_constant_schedule_with_warmup(
+            optimizer=optimizer, num_warmup_steps=self.hparams.num_warmup_steps
+        )
+        return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
