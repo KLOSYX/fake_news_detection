563db24198afe2f2f9000c0ebc96e19a3e6144bb
anbinx <klosyx@outlook.com>
2022-08-16 07:04:43 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="src",
+    name="anbinx-dl-env",
     version="0.0.1",
-    description="Describe Your Cool Project",
-    author="",
-    author_email="",
+    description="My deeplearning environment",
+    author="anbinx",
+    author_email="klosyx@outlook.com",
     url="https://github.com/user/project",  # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
     install_requires=["pytorch-lightning", "hydra-core"],
     packages=find_packages(),
