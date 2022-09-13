f14c92e350227b0ace653a7757889147cb4812f1
ANbinX <46080273+KLOSYX@users.noreply.github.com>
2022-09-13 13:17:35 +0800

    Merge2date (#60)
    * Update pre-commit config updates for jupyter notebooks and flake8 W503
    
    * Fix reformat jupyter notebooks
    
    * Fix logging metrics in DDP mode
    
    * Add shellcheck linter (#427)
    
    * Add Vertex AI integration repo to readme (#440)
    
    * Fix reformat source codes
    
    * Update jupyter notebooks
    
    Co-authored-by: Alexander Tong <alexandertongdev@gmail.com>
    Co-authored-by: Łukasz Zalewski <ashlevegalaxy@gmail.com>
    Co-authored-by: Yongtae723 <50616781+Yongtae723@users.noreply.github.com>

--- scripts/run_seed.sh
+++ scripts/run_seed.sh
@@ -4,8 +4,8 @@
 EXPERIMENT=run_multi_modal_test
 
 for i in {1..10}; do
-    echo "Running seed $i for experiment ${EXPERIMENT}"
-    python src/train.py experiment=${EXPERIMENT} seed=$i model.modal=multi trainer.devices=\'6,7\' trainer.min_epochs=15 trainer.max_epochs=20
-    python src/train.py experiment=${EXPERIMENT} seed=$i model.modal=text trainer.devices=\'6,7\' trainer.min_epochs=15 trainer.min_epochs=15
-    python src/train.py experiment=${EXPERIMENT} seed=$i model.modal=image trainer.devices=\'6,7\' trainer.min_epochs=15 trainer.min_epochs=15
+  echo "Running seed $i for experiment ${EXPERIMENT}"
+  python src/train.py experiment=${EXPERIMENT} seed="$i" model.modal=multi trainer.devices=\'6,7\' trainer.min_epochs=15 trainer.max_epochs=20
+  python src/train.py experiment=${EXPERIMENT} seed="$i" model.modal=text trainer.devices=\'6,7\' trainer.min_epochs=15 trainer.min_epochs=15
+  python src/train.py experiment=${EXPERIMENT} seed="$i" model.modal=image trainer.devices=\'6,7\' trainer.min_epochs=15 trainer.min_epochs=15
 done
