9c08d2dba208e0fc3ad503967756eb640c36e629
ANbinX <46080273+KLOSYX@users.noreply.github.com>
2022-10-17 17:04:10 +0800

    Add simclr image transforms

--- src/datamodules/fake_news_data.py
+++ src/datamodules/fake_news_data.py
@@ -1,14 +1,69 @@
 from pathlib import Path
 from typing import Any, List, Optional, Tuple
 
+import numpy as np
 import pandas as pd
 import torch
 from PIL import Image
+from torch import nn
 from torch.utils.data import Dataset
 from torchvision import transforms
 from transformers import AutoFeatureExtractor, AutoTokenizer
 
 from src.datamodules.components.dm_base import DatamoduleBase
+
+
+class GaussianBlur:
+    """blur a single image on CPU."""
+
+    def __init__(self, kernel_size):
+        radias = kernel_size // 2
+        kernel_size = radias * 2 + 1
+        self.blur_h = nn.Conv2d(
+            3,
+            3,
+            kernel_size=(kernel_size, 1),
+            stride=1,
+            padding=0,
+            bias=False,
+            groups=3,
+        )
+        self.blur_v = nn.Conv2d(
+            3,
+            3,
+            kernel_size=(1, kernel_size),
+            stride=1,
+            padding=0,
+            bias=False,
+            groups=3,
+        )
+        self.k = kernel_size
+        self.r = radias
+
+        self.blur = nn.Sequential(nn.ReflectionPad2d(radias), self.blur_h, self.blur_v)
+
+        self.pil_to_tensor = transforms.ToTensor()
+        self.tensor_to_pil = transforms.ToPILImage()
+
+    def __call__(self, img):
+        img = self.pil_to_tensor(img).unsqueeze(0)
+
+        sigma = np.random.uniform(0.1, 2.0)
+        x = np.arange(-self.r, self.r + 1)
+        x = np.exp(-np.power(x, 2) / (2 * sigma * sigma))
+        x = x / x.sum()
+        x = torch.from_numpy(x).view(1, -1).repeat(3, 1)
+
+        self.blur_h.weight.data.copy_(x.view(3, 1, self.k, 1))
+        self.blur_v.weight.data.copy_(x.view(3, 1, 1, self.k))
+
+        with torch.no_grad():
+            img = self.blur(img)
+            img = img.squeeze()
+
+        img = self.tensor_to_pil(img)
+
+        return img
 
 
 class WeiboDataset(Dataset):
@@ -47,6 +102,10 @@ class WeiboDataset(Dataset):
 
 
 class TwitterDataset(WeiboDataset):
+    def __init__(self, img_path: str, data_path: str) -> None:
+        super().__init__(img_path, data_path)
+        self.transforms = self.get_simclr_pipeline_transform(224)
+
     def __getitem__(self, idx: int) -> Tuple[str, torch.Tensor, torch.Tensor]:
         text = self.data.iloc[idx]["text"]
         img_name = self.data.iloc[idx]["imgs"]
@@ -58,6 +117,23 @@ class TwitterDataset(WeiboDataset):
             self.transforms(img).unsqueeze(0),
             torch.tensor([label], dtype=torch.long),
         )
+
+    @staticmethod
+    def get_simclr_pipeline_transform(size, s=1):
+        """Return a set of data augmentation transformations as described in the SimCLR paper."""
+        color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+        data_transforms = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(size=size),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomApply([color_jitter], p=0.8),
+                transforms.RandomGrayscale(p=0.2),
+                GaussianBlur(kernel_size=int(0.1 * size)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        return data_transforms
 
 
 class Collector:
